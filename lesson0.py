b865080cc74b571d6fe34d765867c404aee49aa9
Dmitry91-tec <nasdi91@yandex.ru>
2024-06-15 20:33:15 +1000

    Add files via upload

---
+++ lesson0.py
@@ -0,0 +1,20 @@
+a = 'Hello'
+print(len(a))
+
+first = 9
+second = 7
+summa = first + second
+print(summa)
+diff = first - second
+print(diff)
+third = 5
+mean = (first + second + third)/3
+print(mean)
+first_string = 'Понедельник'
+second_string = 'Вторник'
+print(first_string + ',', second_string)
+a = 3
+b = 4
+c = 5
+f = (((a*b) + (a*c)) ** 3)/2
+print(f)
